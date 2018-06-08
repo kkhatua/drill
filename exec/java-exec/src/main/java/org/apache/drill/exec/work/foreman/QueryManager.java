e4162ba98d18e209d4351570613c8b34f7b71032
Kunal Khatua <kunal@apache.org>
2018-06-08 15:40:52 -0700

    unused

--- exec/java-exec/src/main/java/org/apache/drill/exec/work/foreman/QueryManager.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/work/foreman/QueryManager.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
